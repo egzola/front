de93d1a2a1b845b2bab467dcfab917075a7ca993
Eduardo Zola <egzola@gmail.com>
2023-10-31 10:27:10 -0300

    Zola commit 2023/3/10 10:27:01.48

--- deploy.bat
+++ deploy.bat
@@ -1,10 +1,12 @@
-set dt=%DATE:~6,4%_%DATE:~3,2%_%DATE:~0,2%__%TIME:~0,2%_%TIME:~3,2%_%TIME:~6,2%
-set dt=%dt: =0%
+set dt=%date:~10,4%%date:~6,2%/%date:~4,2% %time%
+echo %dt%
 
 call npm run build
-rem call npm run start
+
 git add *
 git commit -m "Zola commit %dt%"
 git branch -M main
 git remote add origin https://github.com/egzola/front.git
 git push -u origin main
+
+call npm run start
