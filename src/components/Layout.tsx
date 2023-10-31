52f1de1c0691c88c209e18d42d5482e84153bf15
Eduardo Zola <egzola@gmail.com>
2023-10-31 09:49:00 -0300

    3th commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -29,7 +29,7 @@ export default function Layout(props:any) {
             </main>
 
             <footer>
-                Desenvolvido por Eduardo Zola - 2023
+                Desenvolvido por Eduardo Zola - 2023 / 2024
             </footer>
         </div>
   );
