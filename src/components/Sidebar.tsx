fff963745fae1430c28daf8e44358dc5a67bd94f
Eduardo Zola <egzola@gmail.com>
2023-12-03 17:20:03 -0300

    Zola commit 2023-12-03 17:19:52

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,6 +8,7 @@ import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { TbReportMoney } from 'react-icons/tb';
 import { BiLogIn } from 'react-icons/bi';
 
+import Link from 'next/link';
 
 export default function SideBar(props) {
 
@@ -16,11 +17,12 @@ export default function SideBar(props) {
       <button className="toggleSideBar" onClick={props.toggle}>&#9776;</button>
       <div className="sideBar" style={{width:props.isOpen ? '0px' : '200px',opacity:props.isOpen ? '0.5' : '1'}}>
         <nav>
-           <a href="/"><AiOutlineHome className="menuIcon" />HOME</a>
-           <a href="/about"><AiOutlineInfoCircle className="menuIcon" />ABOUT</a>
-           <a href="/posicao"><TbReportMoney className="menuIcon" />POSICAO</a>
-           <a href="/tasks"><TbReportMoney className="menuIcon" />TASKS</a>
-           <a href="/login"><BiLogIn className="menuIcon" />LOGIN</a>
+           <Link href="/"><AiOutlineHome className="menuIcon" />HOME</Link>
+           <Link href="/about"><AiOutlineInfoCircle className="menuIcon" />ABOUT</Link>
+           <Link href="/posicao"><TbReportMoney className="menuIcon" />POSICAO</Link>
+           <a href="/posicao"><TbReportMoney className="menuIcon" />POSICAO X</a>
+           <Link href="/tasks"><TbReportMoney className="menuIcon" />TASKS</Link>
+           <Link href="/login"><BiLogIn className="menuIcon" />LOGIN</Link>
         </nav>
       </div>
     </>
